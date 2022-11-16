7cec1ba26fa5aacb3d4ac17b6540dbb9181bfba9
root <2915601267@qq.com>
2022-11-16 20:47:04 +0800

    finish server's register

--- lab2/chat/server/register.c
+++ lab2/chat/server/register.c
@@ -1,23 +1,26 @@
+#include "register.h"
+
 /* failed to register, write reason(erno) to client fifo */
 void RegisterFeedback(int fd, int erno)
 {
+	char buf[50];
     switch (erno)
     {
         case 1: // username has been used
-			char* buf = "Register :Failure!Username has been used!";
-			if (write(fd, buf, strlen(buf) == -1)
+			sprintf(buf, "Failure!Username has been used!");
+			if (write(fd, buf, strlen(buf) == -1))
 			{
-				printf("Failed to write fifo %s", fifo);
+				printf("Failed to write fifo\n");
 				perror("");
 				close(fd);
 				exit(EXIT_FAILURE);
 			}
             break;
-        case 2: // Successful register!
-            char* buf = "Register :Successful!";
+		case 2: // Successful register!
+            sprintf(buf, "Successful!");
             if (write(fd, buf, strlen(buf)) == -1)
             {
-                printf("Failed to write fifo %s", fifo);
+                printf("Failed to write fifo\n");
                 perror("");
                 close(fd);
                 exit(EXIT_FAILURE);
@@ -25,24 +28,24 @@ void RegisterFeedback(int fd, int erno)
             break;
     }
 }
+
 /* process register request in fifo(fd) */
 int Register(int fd)
 {
     REGISTERINFO info;
-    if (read(fd, &info, sizeof(REGISTERINFO)))
+    if ( read(fd, &info, sizeof(REGISTERINFO)) == -1 )
     {
-        printf("Failed to read fifo %s\n", FIFO_NAMES[0]);
+        printf("Failed to read fifo %s\n", SERVER_FIFO_NAMES[0]);
         perror("");
         exit(EXIT_FAILURE);
     }
 
     /* open client's fifo for writing only */
-    int fd = open(fifo, O_WRONLY | O_NONBLOCK);
+    fd = open(info.fifo, O_WRONLY | O_NONBLOCK);
     if (fd == -1)
     {
-        printf("Failed to open fifo %s for writing only", fifo);
+        printf("Failed to open fifo %s for writing only\n", info.fifo);
         perror("");
-       // RegisterFeedback(1);
     	exit(EXIT_FAILURE);
 	}
 
@@ -50,15 +53,15 @@ int Register(int fd)
    	
 	/* link database */
 	MYSQL conn;
-	if (mysql_init(&conn))
+	if (mysql_init(&conn) == NULL)
 	{
-		perror("Failed to initialize mysql object");
+		printf("Failed to initialize mysql object\n");
 		close(fd);
 		exit(EXIT_FAILURE);
 	} 
 	if (mysql_real_connect(&conn, _DATABASE_HOST_, _DATABASE_USER_, _DATABASE_PASSWD_, _DATABASE_NAME_, 0, NULL, 0) == NULL)
 	{
-		perror("Failed to connect with database");
+		printf("Failed to connect with database\n");
 		close(fd);
 		exit(EXIT_FAILURE);
 	}
@@ -66,16 +69,17 @@ int Register(int fd)
 	/* check if username is existed in user table  */
 	MYSQL_RES* res;
 	MYSQL_ROW row;
-	char query[200] = "SELECT * FROM user WHERE name=";
+	char query[200] = "SELECT * FROM user WHERE name='";
 	strcat(query, info.username);
+	strcat(query, "';");
+//	printf("%s\n", query);	
 	if (mysql_query(&conn, query))
 	{
-		printf("Failed to query '%s'", query);
-		perror("");
+		printf("Failed to query '%s'\n", query);
 		close(fd);
 		exit(EXIT_FAILURE);
 	}
-	res = mysql_store_result(conn);
+	res = mysql_store_result(&conn);
 	if (mysql_num_rows(res))
 	{
 		RegisterFeedback(fd, 1);
@@ -89,13 +93,12 @@ int Register(int fd)
 	memset(query, 0, sizeof(query));
 	strcat(query, "Insert into user(name, passwd) values('");
 	strcat(query, info.username);
-	strcat(query, "',");
+	strcat(query, "','");
 	strcat(query, info.password);
-	strcat(query, ");");
+	strcat(query, "');");
 	if (mysql_query(&conn, query))
 	{
-		printf("Failed to query '%s'", query);
-		perror("");
+		printf("Failed to query '%s'\n", query);
 		close(fd);
 		exit(EXIT_FAILURE);
 	}
--- lab2/chat/server/register.h
+++ lab2/chat/server/register.h
@@ -1,6 +1,21 @@
 #ifndef _REGISTER_
 #define _REGISTER_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <mysql.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <string.h>
+#include "clientinfo.h"
+#include "fifo.h"
+#define _DATABASE_NAME_ "test"
+#define _DATABASE_USER_ "root"
+#define _DATABASE_PASSWD_ "123456"
+#define _DATABASE_HOST_ "localhost"
+
 void RegisterFeedback(int fd, int erno);
 
 int Register(int fd);
